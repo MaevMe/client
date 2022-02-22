ef4bd928fcd3750a5e46be94d81024344e04743d
Coolicc <o.g.mank@gmail.com>
2022-02-22 10:41:32 -0600

    OAuth

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+  import axios from 'axios'
   import { onMounted } from 'vue'
   import { useRoute } from 'vue-router'
 
   onMounted(() => {
     const route = useRoute()
     console.log(route.query)
+    const code = route.query.code
+    axios.get(`https://api.maev.me/callback?code=${code}`, {
+      withCredentials: true,
+    })
   })
 </script>
