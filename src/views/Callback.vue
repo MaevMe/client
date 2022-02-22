7cbeb154dd00469e78c19c7576ea00bef4af292b
Coolicc <o.g.mank@gmail.com>
2022-02-22 10:18:41 -0600

    OAuth

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+  import { onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
 
-onMounted(() => {})
+  onMounted(() => {
+    const route = useRoute()
+    console.log(route.query)
+  })
 </script>
