f16025aa60acc6ff703c19cd717548e6e760c68b
Norah Maria Hareide Nilsen <norahmaria@Norahs-MBP.domain_not_set.invalid>
2022-02-26 13:16:16 +0100

    Selects in Server VC Settings

--- src/views/Server.vue
+++ src/views/Server.vue
@@ -4,9 +4,9 @@
   import { useRoute } from 'vue-router'
   import type Server from '../types/Server'
 
-  const createChannel = ref()
-  const namingFormat = ref()
-  const category = ref()
+  let createChannel = ref('')
+  let namingFormat = ref('')
+  let category = ref('')
   let server = ref<null | Server>(null)
   let saved = ''
 
@@ -14,12 +14,17 @@
     const route = useRoute()
     const { guildID } = route.params
 
-    console.log(await (await api.get(`/server/${guildID}`, { withCredentials: true })).data)
     server = await (await api.get(`/server/${guildID}`, { withCredentials: true })).data
   })
 
   watch(server, value => {
     console.log('@server', value)
+
+    if (value) {
+      category.value = value.tempVoiceChannels.categoryID
+      namingFormat.value = value.tempVoiceChannels.namingFormat
+      createChannel.value = value.tempVoiceChannels.createChannel
+    }
   })
 
   // const save = async () => {
@@ -43,24 +48,24 @@
 <template>
   <div>{{ server?.name }}</div>
 
-  <div>
+  <div class="settings-card">
     Channel to join:
-    <select v-model="createChannel">
-      <option v-for="channel in server?.voiceChannels" value="channel.id">
+    <select v-model="createChannel" v-if="server?.voiceChannels">
+      <option v-for="channel in server.voiceChannels" value="channel.id">
         {{ channel.name }}
       </option>
     </select>
   </div>
 
-  <div>
+  <div class="settings-card">
     Name format:
     <input v-model="namingFormat" type="text" />
   </div>
 
-  <div>
+  <div class="settings-card">
     Category:
-    <select v-model="category">
-      <option v-for="channel in server?.categories" value="channel.id">
+    <select v-model="category" v-if="server?.categories">
+      <option v-for="channel in server.categories" value="channel.id">
         {{ channel.name }}
       </option>
     </select>
@@ -70,4 +75,8 @@
   {{ saved }} -->
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .settings-card {
+    background: grey;
+  }
+</style>
